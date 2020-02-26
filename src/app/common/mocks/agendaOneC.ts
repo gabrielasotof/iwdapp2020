4b51a6283e09fdce3aaf68c28102f26a541edeff
Natalia Venditto <nataliafrontend@gmail.com>
2020-02-26 20:28:37 +0100

    fix bugs

--- src/app/common/mocks/agendaOneC.ts
+++ src/app/common/mocks/agendaOneC.ts
@@ -2,7 +2,7 @@
 export const AGENDAONE_C = [
   {
     session: 'Stop calling everything a mock',
-    speaker: 'Eli & Inma',
+    speaker: 'Ana María García',
     time: '9:30 - 11:00',
     room: 'ManoMano',
     lang: 'Español/English',
